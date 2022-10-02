8a6fcdf69ad0ac48bad3fa0362ae9d83f15897b6
jtreeves <jackson.t.reeves@gmail.com>
2022-10-02 18:16:54 -0400

    [CartItem] Add buttons to increase or decrease item tally

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -1,12 +1,41 @@
-import { ProductTallyProp } from '../../interfaces'
+import { useContext } from 'react'
+import GlobalStorage from '../../GlobalStorage'
+import { ProductTallyProp, ProductTally } from '../../interfaces'
+import { StorageContext } from '../../types'
+import addProduct from '../../utilities/addProduct'
+import removeProduct from '../../utilities/removeProduct'
 
 function CheckoutProduct({
     product
 }: ProductTallyProp): JSX.Element {
+    const [storage, setStorage] = useContext<StorageContext>(GlobalStorage)
+
+    const increaseTallyInCart = () => {
+        const updatedCart: ProductTally[] = addProduct(product, storage.cart)
+        const updatedStock: ProductTally[] = removeProduct(product, storage.stock)
+
+        setStorage({
+            cart: updatedCart,
+            stock: updatedStock
+        })
+    }
+
+    const decreaseTallyInCart = () => {
+        const updatedCart: ProductTally[] = removeProduct(product, storage.cart)
+        const updatedStock: ProductTally[] = addProduct(product, storage.stock)
+
+        setStorage({
+            cart: updatedCart,
+            stock: updatedStock
+        })
+    }
+
     return (
         <div>
             <p>{product.name}</p>
             <p>{product.tally}</p>
+            <button onClick={increaseTallyInCart}>Increase Amount by 1</button>
+            <button onClick={decreaseTallyInCart}>Decrease Amount by 1</button>
         </div>
     )
 }
