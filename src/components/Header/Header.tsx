7b69590adfe8b037ad989a1d4ca443c4e853d1b7
jtreeves <jackson.t.reeves@gmail.com>
2022-10-02 11:18:23 -0400

    [Header] Add nav bar with links to pages

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,31 @@
+import { Link } from 'react-router-dom'
+
 function Header(): JSX.Element {
     return (
         <header>
             Grocery
+
+            <nav>
+                <ul>
+                    <li>
+                        <Link to='home'>
+                            Home
+                        </Link>
+                    </li>
+                    
+                    <li>
+                        <Link to='products'>
+                            Products
+                        </Link>
+                    </li>
+                    
+                    <li>
+                        <Link to='cart'>
+                            Cart
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
