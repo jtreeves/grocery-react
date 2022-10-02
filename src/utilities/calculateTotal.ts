db86617d6c0b504ee8a6eeda82891099feb324c2
jtreeves <jackson.t.reeves@gmail.com>
2022-10-02 00:05:40 -0400

    [utilities] Flesh out functions

--- src/utilities/calculateTotal.ts
+++ src/utilities/calculateTotal.ts
@@ -1,3 +1,15 @@
-function calculateTotal() {}
+import { Food } from '../interfaces'
+
+function calculateTotal(
+    foods: Food[]
+): number {
+    let total: number = 0
+
+    foods.forEach((food: Food) => {
+        total += food.price
+    })
+
+    return total
+}
 
 export default calculateTotal
